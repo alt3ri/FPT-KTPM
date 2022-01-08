982985bbc127da8c4acef1a1491bbaafd4c6a673
alt3ri <dangtnhhe176816@fpt.edu.vn>
2022-01-09 00:22:58 +0700

    Added 2 new files

---
+++ ws.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+float a, b, c, x, S1, S2;
+
+int main(){
+    
+    printf("Enter a: ");
+    scanf("%f", &a);
+    printf("\nEnter b: ");
+    scanf("%f", &b);
+    printf("\nEnter c: ");
+    scanf("%f", &c);
+    printf("\nEnter x: ");
+    scanf("%f", &x);
+    
+    S1 = a*a*x + b*x + c;
+    S2 = sqrt(b*b - 4*a*c);
+    
+    if ((b*b - 4*a*c) > 0){
+        printf("S1 = %0.2f, S2 = %0.2f", S1, S2);
+    } else {
+        printf("S1 = %0.2f, S2 = 0,00", S1);
+    }
+    
+    return 0;
+}
